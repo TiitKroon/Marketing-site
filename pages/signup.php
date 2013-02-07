ad1bcc2e31371c57bce39344891b043f18d9c9c0
lubuntu <lubuntu@lubuntu-VirtualBox.(none)>
2013-02-07 15:09:48 +0200

    initial commit

---
+++ pages/signup.php
@@ -0,0 +1,6 @@
+<form>
+    First name: <input type="text" name="first name"><br>
+    Last name: <input type="text" name="last name"><br>
+    E-mail: <input type="text" name="e-mail"><br>
+    <input type="submit" value="Submit">
+</form>
